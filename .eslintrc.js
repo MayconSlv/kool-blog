4a1cf3bd32d2d211756cda918ca5d834ac845a3f
mayconslv <mayconslv.contato@gmail.com>
2024-03-23 07:18:14 -0300

    fix: fix merge conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     'no-useless-constructor': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
+    '@typescript-eslint/no-unused-vars': 'off',
     'no-template-curly-in-string': 'off'
   },
   settings: {
