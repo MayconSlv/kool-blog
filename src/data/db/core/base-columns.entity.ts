14abf6869c89855da78cc4dd91fdbde9d67b4a32
mayconslv <mayconslv.contato@gmail.com>
2024-04-17 20:49:56 -0300

    refactor: parsing ID type to string

--- src/data/db/core/base-columns.entity.ts
+++ src/data/db/core/base-columns.entity.ts
@@ -1,7 +1,13 @@
-import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import { CreateDateColumn, Generated, PrimaryColumn, UpdateDateColumn, ValueTransformer } from 'typeorm'
+
+const transformerIdStringNumber: ValueTransformer = {
+  from: (dbToEntityId) => (typeof dbToEntityId === 'number' ? dbToEntityId.toString() : dbToEntityId),
+  to: (entityToDbId) => (typeof entityToDbId === 'string' ? +entityToDbId : entityToDbId),
+}
 
 export class BaseColumnsEntity {
-  @PrimaryGeneratedColumn('increment')
+  @Generated('increment')
+  @PrimaryColumn('int4', { transformer: transformerIdStringNumber })
   id: string
 
   @CreateDateColumn()
